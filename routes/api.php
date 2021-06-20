ff94f71f33cd7315c1a282db49bf72618089efa9
david jacobs <davidjacobs522@gmail.com>
2021-06-20 21:20:25 +0100

    added the login route

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
