422c51e7f7b43320111dea3ea1f0e5ce74a1e7da
david jacobs <davidjacobs522@gmail.com>
2021-06-20 11:18:14 +0100

    finished the validation for the user's credentials

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    //To register a new user
+    public function register(Request $request) {
+        //To validate the user's credentials
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|min:4|confirmed'
+        ]);
+    }
 }
