4ab3c9647fe0f528cea00d00b311e35f91c85a3d
david jacobs <davidjacobs522@gmail.com>
2021-06-20 11:38:41 +0100

    changed the request type to post

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
